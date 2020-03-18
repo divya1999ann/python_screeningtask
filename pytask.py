7cc5ee7732d4d60166ec2202e2150833463c35cd
Divya Ann Kurien <divya.kurien@gmail.com>
2020-03-18 11:58:29 +0530

    some changes

--- pytask.py
+++ pytask.py
@@ -3,23 +3,23 @@ class Participant:
 		self.stack=None
 		self.type_lm=None
 		self.name=name
-		self.time_from=None
+		self.time_fr=None
 		self.time_to=None
 	def addStacks(self,s):
 		self.stack=s
 	def setMentorOrLearner(self,t):
-		if self.t=='L':
+		if t=='L':
 			self.type_lm="Learner"
-		elif self.t=='M':
+		elif t=='M':
 			self.type_lm="Mentor"
 		else:
 			raise Exception()
 	def setAvailableTime(self,t_from,t_to):
 		if self.type_lm=="Mentor":
-			self.time_from=t_from
+			self.time_fr=t_from
 			self.time_to=t_to
 	def getMentor(self,s,t):
 		if self.type_lm=="Mentor":
-			if self.stack==s and (t>=self.time_from and t<=self.time_to):
-				return self.name()
+			if self.stack==s and (t>=self.time_fr and t<=self.time_to):
+				return self.name
 			
